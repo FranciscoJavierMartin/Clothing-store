7b647e90957ba4cde3210fa6e67713b84127836f
Francisco <franciscomartincuenca@protonmail.com>
2019-11-26 22:20:17 +0000

    persistance state added

--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -1,10 +1,20 @@
-import { IGlobalState } from '../../interfaces/states';
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './userReducer';
 import cartReducer from './cartReducer';
+import { IGlobalState } from '../../interfaces/states';
 
-export default combineReducers<IGlobalState>({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart']
+};
+
+const rootReducer = combineReducers<IGlobalState>({
   user: userReducer,
   cart: cartReducer,
 });
 
+export default persistReducer(persistConfig, rootReducer);
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './reducers/rootReducer';
 
 const middlewares = [logger];
 
-const customCompose:any =process.env.NODE_ENV === 'development' ? composeWithDevTools : compose;
+const customCompose: any =
+  process.env.NODE_ENV === 'development' ? composeWithDevTools : compose;
 
-//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export default createStore(
+export const store = createStore(
   rootReducer,
   customCompose(applyMiddleware(...middlewares))
 );
+
+export const persistor = persistStore(store);
